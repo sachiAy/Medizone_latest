95ca09e5f58b6053cfda7878e58741a0bde6e30b
Oshara Amarasiriwardena <amanthi1995@gmail.com>
2019-11-19 22:10:46 +0530

    Create Terms.vue

---
+++ client/src/components/Terms.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div>
+      <br/>
+      <b
+        ><center><h2>Terms & Condition</h2></center></b
+      >
+    </div>
+    <div>
+      <div>
+        <h4 class="detail-header"><b>Definitions: </b></h4>
+      </div>
+      <container>
+      <align-center>
+        <li>
+          <b>Doctor</b> – shall mean a doctor who is a licensed medical
+          practitioner registered under the laws of the Sri Lankan Medical
+          Council.
+        </li>
+        <li>
+          <b>Customer</b> – shall mean any individual who is willingly accepting
+          the T&C stated here to obtain his/her healthcare advice from Doctors
+          partnered with MediZone.
+        </li>
+        <li>
+          <b>Sign Up Process</b>– Customers signing up for MediZone services on
+          the website.
+        </li>
+        <li>
+          <b>Rating </b>- Registered customers rating of the channelled
+          customer.
+        </li>
+        </align-center>
+      </container>
+    </div>
+    <div>
+      <br />
+      <b
+        >General Terms and Conditions applicable for all services offered
+        through MediZone
+      </b>
+    <ol>
+    <li>All customers using MediZone shall be adjudged to have read and agreed to the terms and conditions stated here..</li>
+    <li>All terms and conditions provided herein are applicable at the time of scheduling appointments via the Website.</li>
+    <li>MediZone has the sole authority to extend / discontinue all offers and promotions without any prior notice.</li>
+    <li>All Customers hereby agree to promotional materials / content to be displayed upon registration with MediZone.</li>
+    <li>MediZone has the sole authority to deactivate Customer accounts or Doctor accounts without prior notice in case of fraud or misconduct .</li>
+    <li>MediZone shall not, under any circumstances, be responsible for, inter alia, the quality, accuracy, completeness, value and integrity of the advice provided by the Doctor shall not, under any circumstances, be responsible for, inter alia, the quality, accuracy, completeness, value and integrity of the advice provided by the Doctor or the Rating reviews.</li>
+    <li>MediZone reserves the right to revise these Terms and Conditions at any time, at its discretion.</li>
+<li>Nothing herein expressed or implied is intended to or shall be construed to confer upon or give any person other than the Customer any rights or remedies against MediZone.</li>
+</ol></li>
+    </div>
+
+  <h6 class="detail-header"><b>Registered Customers </b></h6>
+All customers who complete the MediZone Sign Up Process shall be considered as registered customers of MediZone.
+
+<h6 class="detail-header"><b>Guest Customers
+ </b></h6>
+All Customers who have not completed the MediZone Sign Up Process will be considered as guest customers.
+
+<h6 class="detail-header"><b>Registered Clinics </b></h6>
+All clinics who complete the MediZone Sign Up Process shall be considered as registered clinics of MediZone.
+
+  </div>
+</template>
